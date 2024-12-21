6a6c1f9faf981d2c0f9bdd357a3ac6428ed6daba
Rakshithgt <114352972+Rakshithgt@users.noreply.github.com>
2024-12-21 20:01:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,101 @@
+provider "aws" {
+  # access_key = var.aws_access_key
+  # secret_key = var.aws_secret_key
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform20122024"
+    key    = "21122024"
+    region = "ap-south-1"
+  }
+}
+
+
+resource "aws_vpc" "default" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    Name  = var.vpc_name
+    owner = "rakshith"
+  }
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+
+  tags = {
+    Name = var.IGW_name
+  }
+}
+
+resource "aws_subnet" "subnet1-public" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet1_cidr
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = var.public_subnet1_name
+  }
+}
+
+resource "aws_subnet" "subnet2-public" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet2_cidr
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = var.public_subnet2_name
+  }
+}
+
+resource "aws_subnet" "subnet3-public" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet3_cidr
+  availability_zone = "ap-south-1c"
+
+  tags = {
+    Name = var.public_subnet3_name
+  }
+}
+
+resource "aws_route_table" "terraform-public" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+
+  tags = {
+    Name = var.Main_Routing_Table
+  }
+}
+
+resource "aws_route_table_association" "terraform-public" {
+  subnet_id      = aws_subnet.subnet1-public.id
+  route_table_id = aws_route_table.terraform-public.id
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    description = "Allow all inbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
